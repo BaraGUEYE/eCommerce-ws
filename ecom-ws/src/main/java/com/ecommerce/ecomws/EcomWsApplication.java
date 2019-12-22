b8de17d32897f2da13e94fbaf444500fbf8523db
bara <gbara@live.fr>
2019-12-22 22:36:29 +0100

    premier commit

---
+++ ecom-ws/src/main/java/com/ecommerce/ecomws/EcomWsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomWsApplication.class, args);
+    }
+
+}
---
+++ ecom-ws/src/test/java/com/ecommerce/ecomws/EcomWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
